7435cd31bd237748b6607c74f275284a5cd71336
Mangelph <59192337+Mangelph@users.noreply.github.com>
2019-12-24 13:31:48 +0600

    Xyu

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>GunShop</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<div id="logo">
+			<table>
+				<td>
+					<img class="img" src="img/Avengers.png" alt="Marvel">
+				</td>
+				<td class="right">
+					<p>For Additional Information, Contact</p>
+					<p>info@memostra.com</p>
+				</td>
+			</table>
+		</div>
+	</body>
+</html>
